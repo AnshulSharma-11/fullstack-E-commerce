9d5304512cd371546762975c596fb3ef3ee15db1
mantasha <anshulsharma4864@gmail.com>
2026-05-22 22:41:44 +0530

    update product

--- src/components/product/UpdateProduct.jsx
+++ src/components/product/UpdateProduct.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function UpdateProduct() {
   return (
-    <div>UpdateProduct</div>
+    <div>
+<h1>
+    19-05-2026  update product is home work
+</h1>
+
+<div className='d-flex ps-3 pe-3'>
+                <p className='me-3 my-auto'>Logout</p>
+                
+              </div>
+    </div>
   )
 }
